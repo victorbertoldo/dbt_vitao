845ae2900b45a8f637a00ba16ecc3fbd5b81751f
Victor Bertoldo <eu_viktor@hotmail.com>
2024-01-15 17:14:43 -0300

    Update postgres__create_table_as.sql

--- macros/db/postgres/tb_types/postgres__create_table_as.sql
+++ macros/db/postgres/tb_types/postgres__create_table_as.sql
@@ -24,3 +24,48 @@
     {{ sql }}
   );
 {%- endmacro %}
+
+
+{# for newer versions of dbt
+  {% macro postgres__create_table_as(temporary, relation, sql) -%}
+  {%- set unlogged = config.get('unlogged', default=false) -%}
+  {%- set columnar = config.get('columnar', default=false) -%}
+  
+  {{ log("Executing postgres__create_table_as macro", info=True) }}
+  {{ log("Columnar config value: " ~ columnar, info=True) }}
+
+  {%- set sql_header = config.get('sql_header', none) -%}
+
+  {{ sql_header if sql_header is not none }}
+
+  create {% if temporary -%}
+    temporary
+  {%- elif unlogged -%}
+    unlogged
+  {%- endif %} table {{ relation }}
+  {% set contract_config = config.get('contract') %}
+  {% if contract_config.enforced %}
+    {{ get_assert_columns_equivalent(sql) }}
+  {% endif -%}
+  {% if contract_config.enforced and (not temporary) -%}
+      {{ get_table_columns_and_constraints() }} 
+    {%- if columnar == true %}
+      {{ log("Creating columnar table for relation " ~ relation, info=True) }}
+      using columnar
+    {% else %}
+      {{ log("Creating standard table for relation " ~ relation, info=True) }}
+    {%- endif %} ;
+    insert into {{ relation }} (
+      {{ adapter.dispatch('get_column_names', 'dbt')() }}
+    )
+    {%- set sql = get_select_subquery(sql) %}
+  {% else %}
+    as
+  {% endif %}
+  (
+    {{ sql }}
+  );
+{%- endmacro %}
+
+  
+  #}
